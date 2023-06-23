ce2366b39f52c544f0f13b5071911a837d5565f2
Zvi Naor Cohen <zvika96zvika@gmail.com>
2023-06-23 15:06:55 +0300

    Fix tests

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -1,13 +1,13 @@
 import pytest
 
-def test_fail_check():
-    assert 2==1
+# def test_fail_check():
+#     assert 2==1
 
 def test_check():
     assert 1==1
 
-def test_fail_check2():
-    assert 2==1
+# def test_fail_check2():
+#     assert 2==1
 
 def test_check2():
     assert 1==1
