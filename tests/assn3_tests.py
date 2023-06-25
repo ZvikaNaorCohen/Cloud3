9afd18119c3c2a17e271121e7467ba8c42999c51
Zvi Naor Cohen <zvika96zvika@gmail.com>
2023-06-25 09:55:26 +0300

    More tests

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -1,13 +1,84 @@
 import pytest
+import sys
+import connectionController
+from assertions import *
 
-# def test_fail_check():
-#     assert 2==1
 
-def test_check():
-    assert 1==1
+def test_insert_three_dishes():
+    word1 = "orange"
+    word2 = "spaghetti"
+    word3 = "apple pie"
+    response1 = connectionController.http_post("dishes", data={'name': word1})
+    response2 = connectionController.http_post("dishes", data={'name': word2})
+    response3 = connectionController.http_post("dishes", data={'name': word3})
 
-# def test_fail_check2():
-#     assert 2==1
+    assert response1.status_code == 201
+    assert response2.status_code == 201
+    assert response3.status_code == 201
 
-def test_check2():
-    assert 1==1
+    assert response1.json() != response2.json() != response3.json()
+
+'''
+Execute a GET dishes/<orange-ID> request, using the ID of the orange dish.
+The test is successful if:
+ (i) the sodium field of the return JSON object is between .9 and 1.1 and
+ (ii) the return status code from the request is 200.
+'''
+
+
+'''
+Execute a GET /dishes request.
+The test is successful if:
+ (i) the returned JSON object has 3 embedded JSON objects (dishes)
+ (ii) the return status code from the GET request is 200.
+'''
+
+
+def test_insert_dish_doesnt_exist():
+    word1 = "blah"
+    response1 = connectionController.http_post("dishes", data={'name': word1})
+
+    assert response1.json() == -3
+    assert response1.status_code == 404 or response1.status_code == 400 or response1.status_code == 422
+
+
+'''
+Perform a POST dishes request with the dish name “orange”. 
+The test is successful if:
+ (i) the return value is -2 (same dish name as existing dish)
+ (ii) the return status code is 400 or 404 or 422.
+'''
+
+
+def test_insert_dish_already_exists():
+    word1 = "orange"
+    response1 = connectionController.http_post("dishes", data={'name': word1})
+
+    assert response1.json() == -2
+    assert response1.status_code == 404 or response1.status_code == 400 or response1.status_code == 422
+
+
+'''
+Perform a POST /meals request specifying that the meal name is “delicious”, and that it contains:
+an “orange” for the appetizer, “spaghetti” for the main, and “apple pie” for the dessert (note you will need to use their dish IDs).
+The test is successful if:
+(i) the returned ID > 0 
+(ii) the return status code is 201.
+'''
+
+
+'''
+Perform a GET /meals request. 
+The test is successful if:
+(i) the returned JSON object has 1 meal
+(ii) the calories of that meal is between 400 and 500
+(iii) the return status code from the GET request is 200.
+'''
+
+
+'''
+Perform a POST /meals request as in test 6 with the same meal name (and courses can be the same or different). 
+The test is successful if:
+(i) the code is -2 (same meal name as existing meal) 
+(ii) the return status code from the request is 400 or 422.
+'''
---
+++ tests/connectionController.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+URL = "http://localhost:8000"
+
+
+def http_get(resource: str):
+    response = requests.get(url=f"{URL}/{resource}", headers={"Content-Type": "application/json"})
+    return response
+
+
+def http_delete(resource: str):
+    response = requests.delete(url=f"{URL}/{resource}", headers={"Content-Type": "application/json"})
+    return response
+
+
+def http_post(resource: str, data: {}):
+    response = requests.post(url=f"{URL}/{resource}", headers={"Content-Type": "application/json"}, data=json.dumps(data))
+    return response
+
+def http_post_qs(resource: str, query_string: str):
+    response = requests.post(url=f"{URL}/{resource}?name={query_string}", headers={})
+    return response
+
+
+def http_put(resource: str, data: {}):
+    response = requests.put(url=f"{URL}/{resource}", headers={"Content-Type": "application/json"}, data=json.dumps(data))
+    return response
